f534472a81336f8f4e24e12a706aa430600a54fb
Caeman Toombs <toombs.caeman@gmail.com>
2020-07-25 11:03:08 -0400

    simplify, simplify

--- view.py
+++ view.py
@@ -1,25 +1,37 @@
+import collections
 import unittest
+from abc import ABC, abstractmethod
 from operator import getitem, setitem, delitem
+from types import FunctionType
+from typing import Callable
 
-# see also https://python-lenses.readthedocs.io/en/latest/tutorial/optics.html
+"""
+# TODO
+* docs
+* tests
+
+# see also
+* https://python-lenses.readthedocs.io/en/latest/tutorial/optics.html
+"""
+
+__all__ = ['inspect', 'Optic', 'X', 'view']
 
 # sentinels
 none = object()
 delete = object()
 
+# 'types'
+const = 0
+scalar = 1
+add = 2
+mul = 3
+invert = 4
+compose = 5
 
-# TODO
-"""
-* __pow__(n) to repeat an optic n times
-* what would __truediv__ mean? reducing the number of outputs
-* docs
-* tests
-"""
 
-__inspect = ("const", "scalar", "sum", "product", "map")
 def inspect(optic):
     T, *i = optic
-    return tuple((__inspect[T], *(inspect(x) if isinstance(x, Optic) else x for x in i)))
+    return tuple((T, *(inspect(x) if isinstance(x, Optic) else x for x in i)))
 
 
 class Optic(tuple):
@@ -31,106 +43,47 @@ class Optic(tuple):
     X.attr      # lambda x:x.attr               # get attributes
     X[3]        # lambda x:x[3]                 # get items
 
-
-    ## Binding
-    Optics are 'unbound' by default, but can be bound to an object with left shift `<<`
-    A bound optic doesn't take an object in it's call signature.
-    If a bound optic is composed it loses it's biding.
-
-    example:
-    y = X*X << 3    # y is X*X bound to 3
-    y()             # (3, 3)
-
     """
 
-    #   Data Model
-    # The first value determines the execution type
-    # 0: const        get a constant value
-    # 1: scalar       get/set/del single value
-    # 2: sum          get/set/del one of several values
-    # 3: product      get/set/del a fixed number of values > 1
-    # 4: exponential  get/set/del N values
+    def __new__(cls, sequence=None):
+        # Internally, the morphism represents (T, *items)
+        #   where T is an integer representing the morphism 'type'
+        # The constant morphism contains a single value which it always returns
+        # The scalar morphism contains a sequence representing attribute and item operations
+        # All other morphisms contain a sequence of other morphisms.
+        # Hence why default is (scalar,) and not (). It represents the identity function.
 
-    # subsequent items are treated differently in each type.
-    # In const there is a single value.
-    # In scalars they are tuples (T, value) where 0 indicates value is an attribute and 1 an item.
-    # In sum, product, and exponential types they are optics.
-    # Exponential types can contain only a single optic.
+        return tuple.__new__(cls, sequence or (scalar,))
 
-    def __init__(self, _tuple=(), *, obj=none):
-        # must have at least one element (the type)
-        assert len(self)
-        self.__obj = obj
-
-    # BIND
-    def __lshift__(self, other):
-        """Bind"""
-        return type(self)(self, obj=other)
-
-    # HELPERS
-    @property
-    def __T(self):
-        """Get type indicator."""
-        return super().__getitem__(0)
-
-    @property
-    def __items(self):
-        """Get content."""
-        return super().__getitem__(slice(1, None))
-
-    __repr = (
-        lambda a: repr(*a),
-        lambda a: f'X{"".join((f"[{repr(item)}]" if is_item else f".{item}") for is_item, item in a)}',
-        lambda a: f'({"+".join(repr(x) for x in a)})',
-        lambda a: "*".join(repr(x) for x in a),
-        lambda a: f'~({repr(*a)})'
-    )
-    def __repr__(self):
-        T, *a = self
-        return self.__repr[T](a)
+    # The following __functions capture the core functionality
+    # they are dispatched by __call__
 
     # CONST
-    def __const(self, obj, value=none):
-        if value is none:
+    def __const(self, obj=none, value=none):
+        """Const: return a constant value."""
+        _, val = self
+        return val
+
+    __get_op = (getattr, getitem, setattr, setitem, delattr, delitem)
+
+    def __get(self, obj, value=none):
+        """Scalar: return a single value."""
+        if len(self) == 1:
             return obj
 
-    # SCALAR
-    __scalar_op = (getattr, getitem, setattr, setitem, delattr, delitem)
-
-    def __scalar(self, obj, value=none):
-        if not self.__items:
-            return obj
-
-        # reduce over self[:-1]
-        *t, (f, i) = self.__items
+        _, *t, (f, *i) = self
         for f, i in t:
-            obj = self.__scalar_op[f](obj, i)
+            obj = self.__get_op[f](obj, i)
 
-        i = (i,)
         if value is delete:
             f += 4
         elif value is not none:
             f += 2
             i += (value,)
-        return self.__scalar_op[f](obj, *i)
+        return self.__get_op[f](obj, *i)
 
-    def __append(self, *args):
-        return type(self)(tuple(self) + args)
-
-    def __get(self, item, is_item):
-        T, *i = self
-        if T > 1:
-            return type(self)((T, *(self.__scalar_op[is_item](x, item) for x in i)))
-        return self.__append((is_item, item))
-
-    def __getattr__(self, item):
-        return self.__get(item, False)
-
-    def __getitem__(self, item):
-        return self.__get(item, True)
-
-    # SUM
-    def __sum(self, obj, value=none):
+    def __add(self, obj, value=none):
+        """Sum: return the first item to successfully evaluate."""
         for optic in self:
             try:
                 v = optic(obj)
@@ -141,69 +94,98 @@ class Optic(tuple):
                 continue
         raise TypeError
 
-    def __compose(self, other, T):
-        if not isinstance(other, type(self)):
-            other = type(self)((0, other))
-        if self.__T == T:
-            return self.__append(other)
-        return type(self)((T, self, other))
+    def __mul(self, obj, value=none):
+        """Product: return a tuple of values."""
+        _, *items = self
+        return tuple(optic(obj, value) for optic in items)
 
-    def __add__(self, other):
-        return self.__compose(other, 2)
-
-    def __radd__(self, other):
-        return type(self)(0, other) + self
-
-    # PRODUCT
-    def __product(self, obj, value=none):
-        return tuple(optic(obj, value) for optic in self.__items)
-
-    def __mul__(self, other):
-        return self.__compose(other, 3)
-
-    def __rmul__(self, other):
-        return type(self)(0, other) * self
-
-
-    # EXPONENTIAL
-    def __exponential(self, obj, value=none):
+    def __invert(self, obj, value=none):
+        """Map: return a value for each object passed in."""
         _, optic = self
         return tuple(optic(x, value) for x in obj)
 
-    def __invert__(self):
-        return type(self)((4, self))
+    def __rshift(self, obj, value=none):
+        """Compose: chain items together."""
+        _, *optics, last = self
+        for optic in optics:
+            obj = optic(obj)
+        return last(obj, value)
 
-    # REDUCE
-    # def __div(self, obj, value=none):
-    #     L, R = self.__items
+    # dunder methods
 
-    # def __truediv__(self, other):
-    #     if not isinstance(other, type(self)):
-    #         other = type(self)((0, other))
-    #     return type(self)((5, self, other))
+    __repr = (
+        lambda a: repr(*a),
+        lambda a: f'X{"".join((f"[{repr(item)}]" if is_item else f".{item}") for is_item, item in a)}',
+        lambda a: f'({"+".join(repr(x) for x in a)})',
+        lambda a: "*".join(repr(x) for x in a),
+        lambda a: f'~({repr(*a)})',
+        lambda a: ">>".join(repr(x) for x in a),
+    )
 
-    # APPLY
-    __handlers = (__const, __scalar, __sum, __product, __exponential)
+    def __repr__(self):
+        T, *a = self
+        return self.__repr[T](a)
+
+    __handlers = (__const, __get, __add, __mul, __invert, __rshift)
 
     def __call__(self, obj=none, value=none):
-        if self.__obj is none:
-            if obj is none:
-                raise ValueError('must pass object to unbound optic')
-            return self.__handlers[self.__T](self, obj, value)
-        return self.__handlers[self.__T](self, self.__obj, obj)
+        return self.__handlers[super().__getitem__(0)](self, obj, value)
 
-    # DESCRIPTOR
+    def __append(self, other, T):
+        """Append operation."""
+        s_T, *i = self
+        # wrap other when targeting vector types
+        if T > scalar and not isinstance(other, type(self)):
+            other = type(self)((const, other))
+        # map scalar op over items if self is a vector type and target is not
+        if s_T > scalar and T == scalar:
+            is_item, item = other
+            return type(self)((T, *(self.__get_op[is_item](x, item) for x in i)))
+        # reduce nesting if self is already the right type
+        if s_T == T:
+            return type(self)(tuple(self) + (other,))
+        # nest
+        return type(self)((T, self, other))
+
+    def __getattr__(self, item):
+        return self.__append((False, item), scalar)
+
+    def __getitem__(self, item):
+        return self.__append((True, item), scalar)
+
+    def __add__(self, other):
+        return self.__append(other, add)
+
+    def __radd__(self, other):
+        return type(self)((const, other)) + self
+
+    def __mul__(self, other):
+        return self.__append(other, mul)
+
+    def __rmul__(self, other):
+        return type(self)((const, other)) * self
+
+    def __rshift__(self, other):
+        return self.__append(other, compose)
+
+    def __rrshift__(self, other):
+        return type(self)((const, other)) >> self
+
+    def __invert__(self):
+        return type(self)((invert, self))
+
+    # descriptor methods
     def __get__(self, instance, owner):
-        return self()
+        return self(instance)
 
     def __set__(self, instance, value):
-        return self(value)
+        return self(instance, value)
 
     def __delete__(self, instance):
-        return self(delete)
+        return self(instance, delete)
 
 
-def view(obj=none, **kwargs):
+def view(**kwargs):
     """
     Access a set of optics as members of an object.
 
@@ -214,14 +196,13 @@ def view(obj=none, **kwargs):
     v.b = 3
     print(d) # {'a':3}
     """
-    repr_o = '' if obj is none else f'obj={repr(obj)}, '
-    return type('view', (), dict(
-        {k: v if obj is none else v << obj for k, v in kwargs.items()},
-        __repr__=lambda s:f'view({repr_o}**{repr(kwargs)})',
-    ))()
+    T = collections.namedtuple('view', kwargs.keys())
+    # allow binding
+    T.__rrshift__ = lambda s, o: type(s)(*(o >> x for x in s))
+    return T(*kwargs.values())
 
 
-X = Optic((1,))
+X = Optic()
 
 
 class TestOptic(unittest.TestCase):
